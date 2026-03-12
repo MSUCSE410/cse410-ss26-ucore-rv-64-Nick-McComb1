ffeee4f0adff84fa62758fba4e1ae8e50bf96a51
Nick-McComb1 <123035496+Nick-McComb1@users.noreply.github.com>
2026-03-12 17:19:56 +0000

    reimplement sys_task_info and sys_gettimeofday

--- os/syscall.c
+++ os/syscall.c
@@ -37,13 +37,24 @@ uint64 sys_gettimeofday(TimeVal *val, int _tz) // TODO: implement sys_gettimeofd
 	// YOUR CODE
 	val->sec = 0;
 	val->usec = 0;
+	struct proc *p = curr_proc();
 
+    uint64 pa = useraddr(p->pagetable, (uint64)val);
+    if(pa == 0)
+        return -1;
+
+    TimeVal *kval = (TimeVal *)pa;
+
+    uint64 cycle = get_cycle();
+    kval->sec = cycle / CPU_FREQ;
+    kval->usec = (cycle % CPU_FREQ) * 1000000 / CPU_FREQ;
+
+    return 0;
 	/* The code in `ch3` will leads to memory bugs*/
 
 	// uint64 cycle = get_cycle();
 	// val->sec = cycle / CPU_FREQ;
 	// val->usec = (cycle % CPU_FREQ) * 1000000 / CPU_FREQ;
-	return 0;
 }
 
 // TODO: add support for mmap and munmap syscall.
@@ -53,14 +64,19 @@ uint64 sys_gettimeofday(TimeVal *val, int _tz) // TODO: implement sys_gettimeofd
 * LAB1: you may need to define sys_task_info here
 */
 uint64 sys_task_info(TaskInfo *ti){
-	
-	
 	if (!ti) return -1;
-    
-    uint64 now = (get_cycle() * 1000) / CPU_FREQ;
-    *ti = curr_proc()->ti;
-	ti->time = now - ti->start_time;
-	return 0;                
+	struct proc *p = curr_proc();
+    uint64 pa = useraddr(p->pagetable, (uint64)ti);
+    if(pa == 0)
+        return -1;
+
+    TaskInfo *kti = (TaskInfo *)pa;
+
+	uint64 now = (get_cycle() * 1000) / CPU_FREQ;
+    *kti = p->ti;
+	kti->time = now - kti->start_time;
+
+    return 0;             
 }
 
 extern char trap_page[];
